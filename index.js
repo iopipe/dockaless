787a4f41dd1fc1d6781cb54c28e1da934f842705
Jacob Bowers <jbowers2013@my.fit.edu>
2016-06-13 21:08:29 -0400

    fixed scope of 'this' variable & added parameter validation

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ function Dockaless(docker_opts) {
 module.exports = Dockaless
 
 Dockaless.prototype.make_lambda = function(image, cmd) {
+  var dockaless = this
   return function(event, context) {
-    this.docker.run(image, cmd, process.stdout, function(err, data, container) {
+    dockaless.docker.run(image, cmd, process.stdout, function(err, data, container) {
       if (err == null) {
-        ('succeed' in context) ? context.succeed(data) : context(data)
+        (context && 'succeed' in context) ? context.succeed(data) : context(data)
       } else{
-        ('fail' in context) ? context.fail(data) : function() { throw err }
+        (context && 'fail' in context) ? context.fail(data) : function() { throw err }
       }
     })
   }
